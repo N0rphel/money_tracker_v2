b0aa5268327c0b6f5ccdb8f71db1acb4c13b6559
Norphel <tnorphel09@gmail.com>
2026-01-09 12:34:10 +0600

    Add: animation for FAB

--- lib/views/widgets/fab.dart
+++ lib/views/widgets/fab.dart
@@ -9,27 +9,66 @@ class AddFloatingActionButton extends StatefulWidget {
       _AddFloatingActionButtonState();
 }
 
-class _AddFloatingActionButtonState extends State<AddFloatingActionButton> {
+class _AddFloatingActionButtonState extends State<AddFloatingActionButton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 120),
+      reverseDuration: const Duration(milliseconds: 120),
+      lowerBound: 0.0,
+      upperBound: 0.08,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _openSheet(BuildContext context) async {
+    // FAB press animation
+    await _controller.forward();
+    await _controller.reverse();
+
+    // Bottom sheet animation (slower & smoother)
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
+      ),
+      transitionAnimationController: AnimationController(
+        vsync: Navigator.of(context),
+        duration: const Duration(milliseconds: 350),
+        reverseDuration: const Duration(milliseconds: 250),
+      ),
+      builder: (_) => const AddRecordModal(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () {
-        showModalBottomSheet(
-          context: context,
-          isScrollControlled: true,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
-          ),
-          builder: (context) => const AddRecordModal(),
-        );
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Transform.scale(scale: 1 - _controller.value, child: child);
       },
-      backgroundColor: Colors.amber,
-      elevation: 0,
-      shape: const CircleBorder(),
-      child: const Icon(
-        Icons.add,
-        color: Color.fromARGB(255, 72, 57, 57),
-        size: 32,
+      child: FloatingActionButton(
+        onPressed: () => _openSheet(context),
+        backgroundColor: Colors.amber,
+        elevation: 0,
+        shape: const CircleBorder(),
+        child: const Icon(
+          Icons.add,
+          color: Color.fromARGB(255, 72, 57, 57),
+          size: 32,
+        ),
       ),
     );
   }
